e2a82488a9832ec445c1daab45edae0fc3af4630
Rajveer Singh <rajveersinghokay@gmail.com>
2024-06-08 18:00:58 +0530

    Main component added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,11 @@
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
     return (
         <div id="AppComponent">
             <Header />
-            <main>
-                <div className="card">
-                    <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
-                    <p>Essence Mascara Lash Princess</p>
-                </div>
-                <div className="card">
-                    <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
-                    <p>Essence Mascara Lash Princess</p>
-                </div>
-                <div className="card">
-                    <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
-                    <p>Essence Mascara Lash Princess</p>
-                </div>
-                <div className="card">
-                    <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
-                    <p>Essence Mascara Lash Princess</p>
-                </div>
-                <div className="card">
-                    <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
-                    <p>Essence Mascara Lash Princess</p>
-                </div>
-                <div className="card">
-                    <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
-                    <p>Essence Mascara Lash Princess</p>
-                </div>
-                <div className="card">
-                    <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
-                    <p>Essence Mascara Lash Princess</p>
-                </div>
-                <div className="card">
-                    <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
-                    <p>Essence Mascara Lash Princess</p>
-                </div>
-                <div className="card">
-                    <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
-                    <p>Essence Mascara Lash Princess</p>
-                </div>
-                <div className="card">
-                    <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
-                    <p>Essence Mascara Lash Princess</p>
-                </div>
-            </main>
+            <Main />
             <footer>
                 <ul>
                     <li>Home</li>
---
+++ src/components/Main.tsx
@@ -0,0 +1,48 @@
+function Main() {
+    return (
+        <main>
+            <div className="card">
+                <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
+                <p>Essence Mascara Lash Princess</p>
+            </div>
+            <div className="card">
+                <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
+                <p>Essence Mascara Lash Princess</p>
+            </div>
+            <div className="card">
+                <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
+                <p>Essence Mascara Lash Princess</p>
+            </div>
+            <div className="card">
+                <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
+                <p>Essence Mascara Lash Princess</p>
+            </div>
+            <div className="card">
+                <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
+                <p>Essence Mascara Lash Princess</p>
+            </div>
+            <div className="card">
+                <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
+                <p>Essence Mascara Lash Princess</p>
+            </div>
+            <div className="card">
+                <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
+                <p>Essence Mascara Lash Princess</p>
+            </div>
+            <div className="card">
+                <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
+                <p>Essence Mascara Lash Princess</p>
+            </div>
+            <div className="card">
+                <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
+                <p>Essence Mascara Lash Princess</p>
+            </div>
+            <div className="card">
+                <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
+                <p>Essence Mascara Lash Princess</p>
+            </div>
+        </main>
+    );
+}
+
+export default Main;
