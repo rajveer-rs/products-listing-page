ad4959f05e61fe01e4aa3c69d34c1eccd71309ec
Rajveer Singh <rajveersinghokay@gmail.com>
2024-06-08 17:58:38 +0530

    Header component added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
     return (
         <div id="AppComponent">
-            <header>
-                <ul>
-                    <li>Home</li>
-                    <li>Products</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </header>
+            <Header />
             <main>
                 <div className="card">
                     <img src="https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png" />
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <header>
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </header>
+    );
+}
+
+export default Header;
