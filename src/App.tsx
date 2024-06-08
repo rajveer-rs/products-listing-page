245b715d801b42e1a3df633f4c5e2d78a674b6fa
Rajveer Singh <rajveersinghokay@gmail.com>
2024-06-08 12:15:02 +0530

    Header added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 function App() {
-    return <div id="AppComponent">Hello</div>;
+    return (
+        <div id="AppComponent">
+            <header>
+                <ul>
+                    <li>Home</li>
+                    <li>Products</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </header>
+            <main>Body</main>
+            <footer></footer>
+        </div>
+    );
 }
 
 export default App;
